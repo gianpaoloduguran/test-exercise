6c45ac442d6faa735e7141436726503230314ec4
gianpaoloduguran <paoloduguran@gmail.com>
2025-03-30 23:21:53 +0800

    feat: init angular setup.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,15 @@
 import { Routes } from '@angular/router';
+import { ProfileComponent } from './pages/profile/profile.component';
+import { UserListComponent } from './pages/user-list/user-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    pathMatch: 'full',
+    component: ProfileComponent,
+  },
+  {
+    path: 'exercise-2',
+    component: UserListComponent,
+  },
+];
