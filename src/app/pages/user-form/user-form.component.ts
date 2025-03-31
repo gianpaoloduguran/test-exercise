6e39cc8999a29c053660103084c24e355a1f6712
gianpaoloduguran <paoloduguran@gmail.com>
2025-03-31 15:19:37 +0800

    feat: added a user form page.

--- src/app/pages/user-form/user-form.component.ts
+++ src/app/pages/user-form/user-form.component.ts
@@ -1,15 +1,35 @@
+import { NgIf } from '@angular/common';
 import { Component } from '@angular/core';
+import {
+  ReactiveFormsModule,
+  Validators,
+  FormGroup,
+  FormControl,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-user-form',
-  imports: [],
-  template: `
-    <p>
-      user-form works!
-    </p>
-  `,
-  styles: ``
+  imports: [ReactiveFormsModule],
+  templateUrl: './user-form.component.html',
+  styles: ``,
 })
 export class UserFormComponent {
+  profileForm = new FormGroup({
+    name: new FormControl('', Validators.required),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email,
+      Validators.pattern('^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+.[a-zA-Z]{2,}$'),
+    ]),
+  });
 
+  handleSubmit() {
+    alert('Name:' + this.profileForm.value.name);
+  }
+  get name() {
+    return this.profileForm.get('name');
+  }
+  get email() {
+    return this.profileForm.get('email');
+  }
 }
